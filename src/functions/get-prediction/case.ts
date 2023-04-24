a77084e7f9e8d647eeecf997c8462439ba5358ce
Kolesnyk Maksym <kolesnyk.maksym@cmadmins-MacBook-Pro.local>
2023-04-24 17:34:46 +0300

    Adjust prediction fetching

--- src/functions/get-prediction/case.ts
+++ src/functions/get-prediction/case.ts
@@ -21,12 +21,6 @@ async function getPrediction() {
   const predictionFileKey = "predictions/current.json";
   const predictionObject = await getObject(predictionFileKey);
   const prediction = JSON.parse(predictionObject.Body!.toString());
-  const predictionTrainTime = new Date(); // create a new Date object for the current datetime
-  predictionTrainTime.setMinutes(0); // set the minutes to 0
-  predictionTrainTime.setSeconds(0);
 
-  return {
-    last_prediction_time: predictionTrainTime,
-    regions_forecast: prediction,
-  };
+  return prediction;
 }
